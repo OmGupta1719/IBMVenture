5d9a57fbd02b3c8d552a38487aa352b561ea2f6b
OmGupta1719 <guptaom1719@gmail.com>
2025-08-08 02:28:26 +0530

    Vercel Fix Three

--- index.js
+++ index.js
@@ -21,12 +21,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from 'public'
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Serve index.html at root
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
 
 // Helper to get base URL (Vercel & local)
 const getBaseUrl = (req) =>

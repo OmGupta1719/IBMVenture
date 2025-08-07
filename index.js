a784756a385080156f3bfd699b568bd7cf2ce120
OmGupta1719 <guptaom1719@gmail.com>
2025-08-08 02:58:49 +0530

    Vercel Fix Five

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Serve index.html at root
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
 
 
